f3a625d0da035bd8594cf44970efe94af4fa595b
CoocooFroggy <45371102+CoocooFroggy@users.noreply.github.com>
2023-01-11 16:17:08 -0500

    Initial commit

---
+++ test/sandspiel_studio_discord_bot_test.dart
@@ -0,0 +1,8 @@
+import 'package:sandspiel_studio_discord_bot/sandspiel_studio_discord_bot.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
